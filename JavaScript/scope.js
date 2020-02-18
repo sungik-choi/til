05d2526c17a3e04cbcbb1a813516d490c68554d6
Sungik Choi <tjddlr92@gmail.com>
2020-02-18 14:06:12 +0900

    Docs: scope 연습 예제 파일

---
+++ JavaScript/scope.js
@@ -0,0 +1,13 @@
+function countdown() {
+  let i;
+  console.log("Countdown:");
+  for (i = 5; i >= 0; i--) {
+    setTimeout(() => {
+      console.log(i === 0 ? "GO!" : i);
+    }, (5 - i) * 1000);
+  }
+}
+
+// expected : 5 4 3 2 1 GO!
+// real : -1 -1 -1 -1 -1 -1
+countdown();
