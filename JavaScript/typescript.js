5757179297cab22a9b489bebf1e57472b2938419
Sungik Choi <tjddlr92@gmail.com>
2020-04-06 21:38:35 +0900

    Docs: TIL 작성

---
+++ JavaScript/typescript.js
@@ -0,0 +1,5 @@
+"use strict";
+const a = 10;
+const b = 20;
+const add = (a, b) => a + b;
+console.log(add(a, b));
---
+++ JavaScript/typescript.ts
@@ -0,0 +1,4 @@
+const a: number = 10;
+const b: number = 20;
+const add = (a: number, b: number): number => a + b;
+console.log(add(a, b));
